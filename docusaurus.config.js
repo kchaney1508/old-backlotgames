b038abedb1888b61329be2891bcef6ab1073f262
KC1508 <107610683+kchaney1508@users.noreply.github.com>
2024-04-20 15:07:49 -0500

    feat: /discord redirect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,3 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import { themes as prismThemes } from 'prism-react-renderer';
@@ -12,28 +8,36 @@ const config = {
   tagline: 'Open Source Tagline',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
+  // Production URL
   url: 'https://backlot.city',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'kchaney1508', // Usually your GitHub org/user name.
-  projectName: 'backlot.city', // Usually your repo name.
+  organizationName: 'kchaney1508',
+  projectName: 'backlot.city',
   deploymentBranch: 'prod',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   trailingSlash: false,
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
+
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            to: 'https://www.discord.gg/KGdp7X3f63',
+            from: '/discord',
+          },
+        ],
+      },
+    ],
+  ],
 
   presets: [
     [
@@ -42,15 +46,11 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/kchaney1508/backlot.city/tree/prod/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/kchaney1508/backlot.city/tree/prod/',
         },
@@ -64,7 +64,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'Backlot',
