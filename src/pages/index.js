cc5a924cb1b95bfaee1573bb2ecd2e99fe7773aa
KC1508 <107610683+kchaney1508@users.noreply.github.com>
2024-04-22 10:28:01 -0500

    hotfix: desc @ index

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      description="Description will go into a meta tag in <head />">
+      description="Open world roleplay community in GTAV.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
