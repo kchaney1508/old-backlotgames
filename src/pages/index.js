7e69105352e907b8eb8b6780d3d1a5b0b2039ed8
KC1508 <107610683+kchaney1508@users.noreply.github.com>
2024-04-29 11:30:50 -0500

    fix: too dark colors -> new

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,17 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <Heading as="h1" className="hero__title">Backlot</Heading>
+        <p className="hero__subtitle"><b>roleplay community in GTAV</b></p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Join Game
           </Link>
         </div>
       </div>
@@ -29,7 +27,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       description="Open world roleplay community in GTAV.">
