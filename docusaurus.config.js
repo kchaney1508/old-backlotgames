61def4c6c10a8cbc89007a61f52c7f776c6b10fe
KC1508 <107610683+kchaney1508@users.noreply.github.com>
2024-06-11 06:02:50 -0500

    refactor: stash unused pages/delete CSS

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,10 +50,6 @@ const config = {
             to: "/docs/portal",
             from: "/docs",
           },
-          {
-            to: "/staff",
-            from: "/team",
-          },
         ],
       },
     ],
