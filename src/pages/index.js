f6f04c578e0b402f2cd6352c0dcccd5687192cdc
KC1508 <107610683+kchaney1508@users.noreply.github.com>
2024-06-24 14:39:43 -0500

    bump: homepage link

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,9 +20,9 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg homepageButton buttoncss"
-            to="/picklerick"
+            to="/docs/getstarted/fivem"
           >
-            (clicking this button does something superduper cool)
+            Learn About Connecting and Creating a Character
           </Link>
         </div>
       </div>
